44b04bc83c09561f6dd9dd77570979a5004a2389
Julian Kizeno <julianquiceno20@gmail.com>
2020-04-09 14:14:00 +0200

    balloon size changed/ variables added

--- js/balloons.js
+++ js/balloons.js
@@ -3,21 +3,20 @@ class Balloon {
 
     constructor(contx, posX, posY, width, height, canvasSize, vel) {
         this.contx = contx
-        this.posX = posX
+        this.posX = Math.floor(Math.random() * (1240 - 200) + 200)
         this.posY = posY
-        this.width = width
-        this.height = height
+        this.width = 45
+        this.height = 50
         this.canvasSize = {
             width: canvasSize.width,
             height: canvasSize.height
         }
-        this.vel = vel
+        this.vel = Math.random() * (2 - 1) + 1
         this.isBroken = false,
         this.balloon = new Image()
         this.balloon.src = `./images/balloon2.png`
         this.balloonBreak = new Image()
         this.balloonBreak = './images/break-balloon.png'
-
     }
 
     drawInit() {
