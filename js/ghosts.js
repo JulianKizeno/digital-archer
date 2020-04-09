b4813cec9a62fc2dce7733ffe69329215dfee271
Julian Kizeno <julianquiceno20@gmail.com>
2020-04-09 23:10:43 +0200

    Ghosts class created

---
+++ js/ghosts.js
@@ -0,0 +1,28 @@
+
+class Ghosts {
+    constructor(contx, posX, posY, width, height, canvasSize, vel) {
+        this.contx = contx
+        this.posX = posX
+        this.posY = Math.floor(Math.random() * (canvasSize.height + 60 ))
+        this.width = 45
+        this.height = 50
+        this.canvasSize = {
+            width: canvasSize.width,
+            height: canvasSize.height
+        }
+        this.vel = Math.random() * (2 - 1) + 1
+        this.ghost = new Image()
+        this.ghost.src = `./images/ghost1.png`
+    }
+
+    draw() {
+
+        this.contx.drawImage(this.ghost, this.posX, this.posY, this.width, this.height)
+    }
+
+    move() {
+        this.posX -= this.vel 
+    
+    }
+
+}
